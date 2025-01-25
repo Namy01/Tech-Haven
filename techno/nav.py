d541e9d450deaf95726c37565b542d289ca392e7
Namy01 <namy91646@gmail.com>
2025-01-25 12:20:37 +0545

    slight

--- techno/nav.py
+++ techno/nav.py
@@ -3,18 +3,13 @@ from .models import Product, ProductImage, Category, Addcategory, Advertisement,
 def navigations(request):
     tags = Tag.objects.all().order_by("created_at")
     categories_nav = Category.objects.all()[:7]
-<<<<<<< HEAD
     procount = Product.objects.all()
-=======
->>>>>>> 23ae40177c1685d026168855d97ac3cd74c5db18
+
 
     return{
         "tags" : tags,
         "categories_nav" : categories_nav,
-<<<<<<< HEAD
         "procount": procount,
-=======
-        
->>>>>>> 23ae40177c1685d026168855d97ac3cd74c5db18
-        }
+    }
+
     
